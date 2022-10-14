b22e9b95793bc02ca9cc77fdc93c0f505cf167f1
Carlos Alberto <carlosmunizfjunior@gmail.com>
2022-10-14 16:37:41 -0300

    organizing code

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,7 @@ import Home from './pages/home';
 const AppRouter = () => (
   <Routes>
     <Route path='*' element={<div>Erro 404</div>}/>
-    <Route path='/' element={<Home />}>
-      <Route path='/area-logada' element={<div>Logado</div>}>
-        <Route path=':id' />
-      </Route>
-    </Route>
+    <Route path='/' element={<Home />} />
   </Routes>
 );
 
