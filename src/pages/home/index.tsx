bffcaf2231d64921a9e32054f03ad4518aa8fbb8
Carlos Alberto <carlosmunizfjunior@gmail.com>
2022-10-14 16:37:12 -0300

    using recoilState

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,45 @@
-import { Outlet, useParams } from "react-router";
+import { useState } from "react";
+import { useRecoilState, useRecoilStateLoadable, useRecoilValueLoadable } from "recoil";
 
+//recoil: atoms
+import { atomPokemon } from "../../store/atoms";
+
+//recoil: selectors
+import { selectorGetPokemon } from "../../store/selectors";
+
+
+// ::
 const HomePage = () => {
-  const { id } = useParams();
+  const [searchPokemon, setSearchPokemon] = useState("");
+
+  const [pokemon, setPokemon] = useRecoilState(atomPokemon);
+
+  const getLoadablePokemon = useRecoilValueLoadable(selectorGetPokemon)
+
+  console.log(getLoadablePokemon.contents)
+
   return (
     <div>
-      Olá Mundo! <br />
-      {id}
-      <Outlet />
+      <input
+        type="text"
+        onChange={(event) => setSearchPokemon(event.target.value)}
+      />
+
+      <button onClick={() => setPokemon(searchPokemon)}>Procurar</button>
+      {getLoadablePokemon.state === "loading" && <div>loading... </div>}
+      {getLoadablePokemon.state === "hasValue" &&
+        getLoadablePokemon.contents !== undefined && (
+          <div>
+            <img
+              width="150px"
+              src={getLoadablePokemon.contents.sprites.front_default}
+              alt={`pokemon-${getLoadablePokemon.contents.name}`}
+            />
+
+            <h3>{getLoadablePokemon.contents.name}</h3>
+          </div>
+        )}
+      {pokemon}
     </div>
   );
 };
